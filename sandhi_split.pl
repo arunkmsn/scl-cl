f16225d9abde901dd2c7ac070cea4b15ead2c2b0
Arun Kumar M S <arunkmsn@gmail.com>
2019-06-23 18:00:37 +0000

    sandhi_split.pl

--- sandhi_split.pl
+++ sandhi_split.pl
@@ -20,13 +20,36 @@ require "$GlblVar::SCLINSTALLDIR/converters/convert.pl";
 # word:
 # sandhi_type: b
 # encoding: Unicode
-    
-my $word=$ARGV[0];
+
+my $filename=$ARGV[0];
 my $sandhi_type=$ARGV[1];
 my $encoding=$ARGV[2];
 
-chdir("$GlblVar::SCLINSTALLDIR/SHMT");
-system("./prog/sandhi_splitter/web_interface/callsandhi_splitter.pl $GlblVar::SCLINSTALLDIR $encoding $word $sandhi_type $$");
-print `/bin/cat $GlblVar::TFPATH/seg_$$/most_probable_output_utf.txt`, "\n\n";
-print `/bin/cat $GlblVar::TFPATH/seg_$$/all_possible_outputs.txt`;
 
+chdir("$GlblVar::SCLINSTALLDIR/SHMT");
+open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+
+while(my $word=<$fh>) {
+  chomp($word);
+  system("./prog/sandhi_splitter/web_interface/callsandhi_splitter.pl $GlblVar::SCLINSTALLDIR $encoding $word $sandhi_type $$");
+  my $sandhi = `/bin/cat $GlblVar::TFPATH/seg_$$/most_probable_output_utf.txt`;
+  my $others = `/bin/cat $GlblVar::TFPATH/seg_$$/all_possible_outputs.txt`;
+
+  while($sandhi =~ /<\s*a\s*title\s*=\s*"(.*)"\s*>/) {
+    $sandhi =~ s/(.*)<\s*a\s*title\s*=\s*"(.*)"\s*>(.*)/ $1 [$2] $3 /g;
+  }
+  while($others =~ /<\s*a\s*title\s*=\s*"(.*)"\s*>/) {
+    $others =~ s/(.*)<\s*a\s*title\s*=\s*"(.*)"\s*>(.*)/ $1 [$2] $3 /g;
+  }
+
+  while($sandhi =~ /<\s*\/a\s*>/) {
+    $sandhi =~ s/(.*)<\s*\/a\s*>(.*)/ $1 $2 /g;
+  }
+  while($others =~ /<\s*\/a\s*>/) {
+    $others =~ s/(.*)<\s*\/a\s*>(.*)/ $1 $2 /g;
+  }
+
+  $sandhi =~ s/^\s+|\s+$//g;
+  chop($sandhi);
+  print $sandhi . "\n";
+}
