a6186e852224f7fbd7ec6fdec154beabb00d2752
Arun Kumar M S <arunkmsn@gmail.com>
2019-06-23 08:29:40 +0000

    changes for taking in files.

--- morph.pl
+++ morph.pl
@@ -6,13 +6,17 @@ use warnings;
 require "/home/vagrant/scl/paths.pl";
 require "$GlblVar::SCLINSTALLDIR/converters/convert.pl";
 
-my $word=$ARGV[0];
+my $filename=$ARGV[0];
 my $encoding=$ARGV[1];
+open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
 
-my $word_wx = &convert($encoding,$word,$GlblVar::SCLINSTALLDIR);
-chomp($word_wx);
-chdir("$GlblVar::SCLINSTALLDIR/SHMT");
+while(my $word=<$fh>) {
+  chomp($word);
+  my $word_wx = &convert($encoding,$word,$GlblVar::SCLINSTALLDIR);
+  chomp($word_wx);
+  chdir("$GlblVar::SCLINSTALLDIR/SHMT");
 
-my $ans = `./prog/morph/webrun_morph.sh $word_wx`;
-$ans = join "\n", split("/", $ans);
-print STDOUT $ans;
+  my $ans = `./prog/morph/webrun_morph.sh $word_wx`;
+  $ans = join "\n", split("/", $ans);
+  print STDOUT $ans, "\n";
+}
